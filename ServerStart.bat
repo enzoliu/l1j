6671ae4fdc026b201a7e3239ae65996219565c03
EnzoLiu <item.search@gmail.com>
2022-06-11 11:37:33 +0800

    feat: add support linux (#1)
    Co-authored-by: Enzo Liu <enzoliu@outlook.com>

--- ServerStart.bat
+++ ServerStart.bat
@@ -1,3 +1,4 @@
-@java -Xmx512m -Xincgc -cp l1jserver.jar;lib\c3p0-0.9.1.1.jar;lib\mysql-connector-java-5.1.5-bin.jar;lib\javolution.jar l1j.server.Server
-cls
-ServerStart.bat
+@ECHO OFF
+java -Xmx512m -Xincgc -jar l1jserver.jar
+
+PAUSE
